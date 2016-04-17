4fa6dfc724afe0c90147b346124cd1668b776b6f
Connor Edwards <Connor Edwards>
2016-04-17 21:29:08 +0100

    Added more routes

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var config = require("./config/app-config.js");
 var app_name = config.app.name,
     app_port = config.app.port;
 
+// Getting the MongoDB models
+var User = require('./app/models/user.js');
+
 // Startup message
 console.log(app_name + " is starting...");
 
@@ -19,6 +22,29 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
     res.render("home", { title: app_name + " | Home", app_name: app_name });
 });
+
+// Profile
+app.get('/profile', checkLogin, function(req, res) {
+    res.render('profile', { user: req.user });
+});
+
+// Google+ Login
+app.get("/auth/google", passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get("/auth/google/callback", passport.authenticate('google', { successRedirect: '/profile', failureRedirect: '/' }));
+
+// Logout
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+// Function to check that the user is logged in
+function checkLogin(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next(); // They are, carry on
+    }
+    res.redirect('/login'); // They're not, redirect to login page
+}
 
 // Starting the app
 app.listen(app_port, function() {
