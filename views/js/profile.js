16029e3ce738950169913be847eaac57eab99539
FroopleXP <connor@frooplexp.com>
2016-04-24 12:02:17 +0100

    Code tidy up

--- views/js/profile.js
+++ views/js/profile.js
@@ -19,43 +19,31 @@ $(document).ready(function() {
 
     // New user list
     socket.on('user_list', function(data) {
-
         var data_to_add = "";
-
         // Looping through the list and rendering to page
         $.each(data, function(index, value) {
             data_to_add += "<li class='list-group-item " + value.id + "'><img src='" + value.profile_pic + "' class='img-circle' height='30px' width='30px' style='margin-right: 10px;'>" + value.name + "</li>";
         });
-
         // Rendering data to page
         user_table.html(data_to_add);
-
     });
 
     // New user has joined the chat
     socket.on('new_user', function(data) {
-
-        console.log(data);
-
         // Adding the new user to the list
         user_table.prepend("<li class='list-group-item " + data.id + "'><img src='" + data.profile_pic + "' class='img-circle' height='30px' width='30px' style='margin-right: 10px;'>" + data.name + "</li>");
-
     });
 
     // A user has disconnected from the chat
     socket.on('lost_user', function(data) {
-
         var id = data.toString(),
             to_remove = "." + id;
-
         // Removing the user from the table
         $(to_remove).remove();
-
     });
 
     // New message
     socket.on('new_message', function(data) {
-
         // Checking who the sender of the message is
         if (data.sender_id === user_id) { // It's their own message
             render_message(data);
@@ -63,10 +51,8 @@ $(document).ready(function() {
             $('#chatAudio')[0].play();
             render_message(data); // Playing the notification tone
         }
-
         // Auto scrolling the message box
         message_box.animate({"scrollTop": message_box[0].scrollHeight}, "slow");
-
     });
 
     // Welcome message
